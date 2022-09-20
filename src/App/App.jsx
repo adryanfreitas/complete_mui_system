32b055638f42ddf3d99f14b42f0f67c8bbc16817
Adryan da Luz Freitas <Adryan.luz.freitas@gmail.com>
2022-09-20 15:55:21 -0400

    feat: :sparkles: Add inicial project files

---
+++ src/App/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import SideMenu from "../components/SideMenu";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  appMain: {
+    paddingLeft: "320px",
+    width: "100%",
+  },
+});
+
+function App() {
+  const classes = useStyles();
+  return (
+    <>
+      <SideMenu />
+      <div className={classes.appMain}>here we go</div>
+    </>
+  );
+}
+
+export default App;
