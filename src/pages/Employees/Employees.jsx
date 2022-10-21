c42b7e1b5b11f236ae79f91ad45181ef303c00c0
Adryan <adryan.luz.freitas@gmail.com>
2022-10-21 10:13:04 -0400

    feat: :sparkles: Add Employees Page

--- src/pages/Employees/Employees.jsx
+++ src/pages/Employees/Employees.jsx
@@ -1,13 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import EmployeeForm from "./EmployeeForm";
 import PageHeader from "../../components/PageHeader";
 import PeopleOutlineTwoToneIcon from "@material-ui/icons/PeopleOutlineTwoTone";
-import { Paper, makeStyles } from "@material-ui/core";
+import {
+  Paper,
+  makeStyles,
+  TableBody,
+  TableRow,
+  TableCell,
+  Toolbar,
+  InputAdornment,
+} from "@material-ui/core";
+import useTable from "../../components/controls/useTable";
+import * as employeeService from "../../services/employeeService";
+import Controls from "../../components/controls/Controls";
+import { Search } from "@material-ui/icons";
 
-const useStyles = makeStyles();
+const useStyles = makeStyles((theme) => ({
+  pageContent: {
+    margin: theme.spacing(5),
+    padding: theme.spacing(3),
+  },
+  searchInput: {
+    width: "75%",
+  },
+}));
+
+const headCells = [
+  { id: "male", label: "Employee Name" },
+  { id: "female", label: "Email Address (Personal)" },
+  { id: "other", label: "Mobile Number" },
+  { id: "other", label: "Department", disableSorting: true },
+];
 
 export default function Employees() {
-  const classes = useStyles;
+  const classes = useStyles();
+  const [records, setRecords] = useState(employeeService.getAllEmployees());
+  const [filterFn, setFilterFn] = useState({
+    fn: (items) => {
+      return items;
+    },
+  });
+
+  const { TblContainer, TblHead, TblPagination, recordsAfterPadingAndSorting } =
+    useTable(records, headCells, filterFn);
+
+  const handleSearch = (e) => {
+    let target = e.target;
+    setFilterFn({
+      fn: (items) => {
+        if (target.value === "") return items;
+        else
+          return items.filter((x) =>
+            x.fullName.toLowerCase().includes(target.value)
+          );
+      },
+    });
+  };
 
   return (
     <>
@@ -16,8 +65,36 @@ export default function Employees() {
         subTitle="Form design with validation"
         icon={<PeopleOutlineTwoToneIcon fontSize="large" />}
       />
-      <Paper classes={classes.pageContent}>
-        <EmployeeForm />
+      <Paper className={classes.pageContent}>
+        {/* <EmployeeForm /> */}
+        <Toolbar>
+          <Controls.Input
+            label="Search Employees"
+            className={classes.searchInput}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Search />
+                </InputAdornment>
+              ),
+            }}
+            onChange={handleSearch}
+          />
+        </Toolbar>
+        <TblContainer>
+          <TblHead />
+          <TableBody>
+            {recordsAfterPadingAndSorting().map((item) => (
+              <TableRow key={item.id}>
+                <TableCell>{item.fullName}</TableCell>
+                <TableCell>{item.email}</TableCell>
+                <TableCell>{item.mobile}</TableCell>
+                <TableCell>{item.department}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </TblContainer>
+        <TblPagination />
       </Paper>
     </>
   );
