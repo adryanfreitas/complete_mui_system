c83bb388b14b4033abee6c3d929410e55814aa30
Adryan <adryan.luz.freitas@gmail.com>
2022-10-21 10:14:12 -0400

    feat: :sparkles: Add EmployeeForm Page

--- src/pages/Employees/EmployeeForm.jsx
+++ src/pages/Employees/EmployeeForm.jsx
@@ -23,39 +23,36 @@ const initialFValues = {
 };
 
 export default function EmployeeForm() {
-
-  const validate =(fieldValues = values)=>{
-    let temp = {...errors}
-    if('fullName' in fieldValues)
-    temp.fullName = fieldValues.fullName? "" :"This field is required."
-    if('email' in fieldValues)
-    temp.email = (/$^|.+@.+..+/).test(fieldValues.email) ? "" : "Email is not required."
-    if ('mobile' in fieldValues)
-            temp.mobile = fieldValues.mobile.length > 9 ? "" : "Minimum 10 numbers required."
-    if ('departmentId' in fieldValues)
-            temp.departmentId = fieldValues.departmentId.length !== 0 ? "" : "This field is required."
+  const validate = (fieldValues = values) => {
+    let temp = { ...errors };
+    if ("fullName" in fieldValues)
+      temp.fullName = fieldValues.fullName ? "" : "This field is required.";
+    if ("email" in fieldValues)
+      temp.email = /$^|.+@.+..+/.test(fieldValues.email)
+        ? ""
+        : "Email is not valid.";
+    if ("mobile" in fieldValues)
+      temp.mobile =
+        fieldValues.mobile.length > 9 ? "" : "Minimum 10 numbers required.";
+    if ("departmentId" in fieldValues)
+      temp.departmentId =
+        fieldValues.departmentId.length !== 0 ? "" : "This field is required.";
     setErrors({
-      ...temp
-    })
-  
-    if(fieldValues === values)
-      return Object.values(temp).every(x => x === "")
-  }
+      ...temp,
+    });
 
-  const { 
-    values, 
-    setValues, 
-    errors, 
-    setErrors, 
-    handleInputChange, 
-    resetForm 
-  } = useForm(initialFValues,true, validate);
+    if (fieldValues === values)
+      return Object.values(temp).every((x) => x === "");
+  };
 
-  const handleSubmit = e => {
-    e.preventDefault()
-    if ( validate())
-    window.alert("testing")
-  }
+  const { values, setValues, errors, setErrors, handleInputChange, resetForm } =
+    useForm(initialFValues, true, validate);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validate()) employeeService.insertEmployee(values);
+    resetForm();
+  };
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -75,14 +72,14 @@ export default function EmployeeForm() {
             onChange={handleInputChange}
             error={errors.email}
           />
-                    <Controls.Input
+          <Controls.Input
             label="Mobile"
             name="mobile"
             value={values.mobile}
             onChange={handleInputChange}
             error={errors.mobile}
           />
-                    <Controls.Input
+          <Controls.Input
             label="City"
             name="city"
             value={values.city}
@@ -105,7 +102,7 @@ export default function EmployeeForm() {
             options={employeeService.getDepartmentCollection()}
             error={errors.departmentId}
           />
-          <Controls.DatePicker 
+          <Controls.DatePicker
             name="hireDate"
             label="Hire Date"
             value={values.hireDate}
@@ -119,15 +116,9 @@ export default function EmployeeForm() {
           />
 
           <div>
-             <Controls.Button
-                type="submit"
-                text="Submit" />
-                <Controls.Button
-                  text="Reset"
-                  color="default" 
-                  onClick={resetForm}/>
-          </div>    
-
+            <Controls.Button type="submit" text="Submit" />
+            <Controls.Button text="Reset" color="default" onClick={resetForm} />
+          </div>
         </Grid>
       </Grid>
     </Form>
